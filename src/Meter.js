eb5251b3bbab68794be8bb25febe2169d90c8352
Timo Laak <timo.laak@futurice.com>
2016-09-18 16:41:16 +0200

    Very quick and dirty solution for showing the data

---
+++ src/Meter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import gradient from './images/red-green-gradient.svg';
+import arrow from './images/arrow.svg';
+
+export const Meter = React.createClass({
+  meterData: {
+    'min': 383,
+    'value': 640,
+    'max': 897,
+    'format': 'currency',
+    'unit': 'GBP'
+  },
+
+  // convert 897 to 100%
+  // convert 383 to 0%
+
+  render() {
+    const pointerPosition =
+      (this.meterData.value - this.meterData.min) / (this.meterData.max - this.meterData.min) * 100;
+
+    return (
+      <div className="lizardometer">
+        <img src={gradient} role="presentation" className="lizardometer__background" />
+        <img src={arrow} role="presentation"
+          className="lizardometer__pointer" style={{left: `calc(${pointerPosition}% - 10px`}} />
+      </div>
+    );
+  }
+});
+
+export default Meter;
