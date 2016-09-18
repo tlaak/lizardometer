9824a70938b45bb2521de3fc206a34bbde5fdcab
Timo Laak <timo.laak@futurice.com>
2016-09-18 18:46:27 +0200

    Render the pointer and the data

--- src/Meter.js
+++ src/Meter.js
@@ -2,6 +2,10 @@ import React from 'react';
 import arrow from './images/arrow.svg';
 import './Meter.css';
 import { getData } from './api';
+import {
+  getPointerPosition,
+  getCurrencySymbol
+} from './helpers';
 
 export const Meter = React.createClass({
   // Default values for the meter
@@ -16,25 +20,48 @@ export const Meter = React.createClass({
   },
 
   componentDidMount() {
+    // Gets the data from the API and assigns the response to the component state
     getData()
       .then((response) => {
         this.setState({
           min: response.min,
           max: response.max,
-          value: response.value
+          value: response.value,
+          format: response.format,
+          unit: response.unit
         });
       })
       .catch((error) => {
+        // Not sure what kind of errors it's possible to get...
         console.error('Error when fetching meter data', error);
       });
   },
 
   render() {
-    const pointerPosition =
-      (this.state.value - this.state.min) /
-      (this.state.max - this.state.min) * 100;
+    // Calculate the pointer position as percentage
+    const pointerPosition = getPointerPosition(this.state.value, this.state.min, this.state.max);
+    const { unit, value } = this.state;
+
+    // Get a currency symbol for the unit
+    const symbolOrCurrencyCode = getCurrencySymbol(unit);
+
+    // Show the currency symbol as prefix if it's $ or £
+    const currencyPrefix = (symbolOrCurrencyCode === '$' || symbolOrCurrencyCode === '£')
+      ? symbolOrCurrencyCode
+      : '';
+
+    // Show the currency symbol or the currency code as suffix if it's not $ or £
+    const currencySuffix = (symbolOrCurrencyCode === '$' || symbolOrCurrencyCode === '£')
+      ? ''
+      : symbolOrCurrencyCode;
 
     return (
+      <div className="lizardometer__container">
+        <div className="lizardometer__values">
+          <span className="lizardometer__value--min">{this.state.min}</span>
+          <span className="lizardometer__value">{currencyPrefix}{value}{currencySuffix}</span>
+          <span className="lizardometer__value--max">{this.state.max}</span>
+        </div>
         <img src={arrow} role="presentation"
           className="lizardometer__pointer" style={{left: `calc(${pointerPosition}% - 10px`}} />
       </div>
